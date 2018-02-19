419d59744d08101632a3ddb23321ded96a12a2e0
mattoop <mattoop@IVNMATTOOPLT01.ad.skynet>
2018-02-19 12:48:56 -0800

    AddTwoNumbers

--- src/linkedlist/AddTwoNumbers.java
+++ src/linkedlist/AddTwoNumbers.java
@@ -13,11 +13,11 @@ public class AddTwoNumbers {
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
         ListNode l1 = new ListNode(2);
         l1.next = new ListNode(4);
-        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(3);
         
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
-        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(4);
         printNode(addTwoNumbers.addTwoNumbers(l1, l2));
     }
     
@@ -31,30 +31,35 @@ public class AddTwoNumbers {
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        ListNode outputNode = null;    ListNode temp = null;
-        int carry=0;    int firstVal=0; int secondVal = 0;  int sum = 0;
+        ListNode outputNode = null;    ListNode temp = null;    ListNode prev = null;
+        int carry=0; int sum = 0;
         
         while(l1 != null || l2 != null) {
             sum = (l1 != null? l1.val:0) + (l2 != null ? l2.val: 0) + carry;
-            if(sum >10) 
+            if(sum >= 10) { 
                 carry=1;
+            } else {
+                carry=0;
+            }
             sum = sum % 10;
             temp = new ListNode(sum);
             if(outputNode == null) {
                 outputNode = temp;
             } else {
-                outputNode.next = temp;
+                prev.next = temp;
             }
+            prev = temp;
             if(l1 != null) {
                 l1 = l1.next;
             }
-            if(l2.next != null) {
+            if(l2 != null) {
                 l2 = l2.next;
             }
+            
         }
         
         if(carry > 0) {
-            outputNode.next = new ListNode(carry);
+            temp.next = new ListNode(carry);
         }
         
         return outputNode;
